6946f8d4581663a5189e4ef84b3442ba7fe3e6a8
krd0197 <62950517+krd0197@users.noreply.github.com>
2021-12-18 16:30:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
   name: 'Kris',
   imageBackground: false,
   openInNewTab: true,
-  twelveHourFormat: false,
+  twelveHourFormat: true,
 
   // Greetings
   greetingMorning: 'Good morning!',
@@ -48,7 +48,7 @@ const CONFIG = {
       id: '2',
       name: 'Mail',
       icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      link: 'https://mail.google.com/',
     },
     {
       id: '3',
@@ -70,9 +70,9 @@ const CONFIG = {
     },
     {
       id: '6',
-      name: 'Odysee',
+      name: 'YouTube',
       icon: 'youtube',
-      link: 'https://odysee.com/',
+      link: 'https://youtube.com/',
     },
   ],
 
